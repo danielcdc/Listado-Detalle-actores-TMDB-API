d86c5ae1b4e191da539e0646b489df24a9a6973c
Dani Cano <danielcdc8591@gmail.com>
2021-03-08 18:59:59 +0100

    Creación del proyecto

---
+++ app/src/main/java/com/danielcano/listado_detalle_actores/ui/dashboard/DashboardViewModel.kt
@@ -0,0 +1,13 @@
+package com.danielcano.listado_detalle_actores.ui.dashboard
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class DashboardViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is dashboard Fragment"
+    }
+    val text: LiveData<String> = _text
+}
